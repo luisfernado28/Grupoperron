e70c12cdedbec6b161ffead590dee9e2277179ff
gabojm <gabojarmuaz@gmail.com>
2017-03-06 20:28:19 -0400

    Creacion resto activities

---
+++ app/src/main/java/com/example/luisfernando/superhipermegaperron/ElectroActivity.java
@@ -0,0 +1,13 @@
+package com.example.luisfernando.superhipermegaperron;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ElectroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_electro);
+    }
+}
---
+++ app/src/main/java/com/example/luisfernando/superhipermegaperron/MusicaActivity.java
@@ -0,0 +1,13 @@
+package com.example.luisfernando.superhipermegaperron;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_musica);
+    }
+}
