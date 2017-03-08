1cd2a718795329a1d2ed6b6226eb054be97c87ad
Sergio <sergiomarzana1@gmail.com>
2017-03-08 19:12:52 -0400

    Notificacion chingona

--- app/src/main/java/com/example/luisfernando/superhipermegaperron/MenuActivity.java
+++ app/src/main/java/com/example/luisfernando/superhipermegaperron/MenuActivity.java
@@ -1,8 +1,12 @@
 package com.example.luisfernando.superhipermegaperron;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -30,6 +34,22 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
         HHITEMS_BUTTON = (ImageButton)findViewById(R.id.HHITEMS_BUTTON);
         MUSIC_BUTTON = (ImageButton)findViewById(R.id.MUSIC_BUTTON);
         LOG_OUT_BUTTON = (ImageButton)findViewById(R.id.LOG_OUT_BUTTON);
+
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+
+        PendingIntent pIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+
+        Notification notification = new NotificationCompat.Builder(MenuActivity.this)
+                .setContentTitle("BIENVENIDO")
+                .setContentText("OYE VOLVÉ")
+                .setSmallIcon(R.drawable.notification)
+                .setContentIntent(pIntent)
+                .setDefaults(Notification.DEFAULT_SOUND)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification);
+
 
         TV_BUTTON.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +93,7 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(logOut);
             }
         });
+
     }
 
 
