1233a36d18613a20582652e0bb3def431a46f44e
luisfernado28 <Ilovemusic28>
2017-03-11 12:34:57 -0400

    tvActivity con lista de Items

--- app/src/main/java/com/example/luisfernando/superhipermegaperron/TVActivity.java
+++ app/src/main/java/com/example/luisfernando/superhipermegaperron/TVActivity.java
@@ -2,6 +2,14 @@ package com.example.luisfernando.superhipermegaperron;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class TVActivity extends AppCompatActivity {
 
@@ -9,5 +17,31 @@ public class TVActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv);
+
+        ListView lista=(ListView)findViewById(R.id.listview);
+
+        //Agregamos algunas filas
+        ArrayList<Item> items=new ArrayList<Item>();
+        items.add(new Item(1, "Icono", "Esta es la descripcion del icono", android.R.drawable.ic_menu_camera));
+        items.add(new Item(1, "Play", "Esta es la descripcion de Play", android.R.drawable.ic_media_play));
+        items.add(new Item(1, "Galeria", "Esta es la descripcion de Galeria", android.R.drawable.ic_menu_gallery));
+        items.add(new Item(1, "Nuevo", "Esta es la descripcion de Nuevo", android.R.drawable.ic_menu_more));
+        items.add(new Item(1, "poi", "asdfasdfasdfaso", android.R.drawable.ic_dialog_info));
+
+
+        //Instanciamos el adaptador
+        AdaptadorItem adaptador=new AdaptadorItem(TVActivity.this, items);
+        //Utilizamos el adaptador
+        lista.setAdapter(adaptador);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> array, View vista, int posicion,
+                                    long id) {
+                TextView titulo=(TextView)vista.findViewById(R.id.titulo);
+                Log.e("Item seleccionado", titulo.getText().toString());
+            }
+        });
+
+
     }
 }
